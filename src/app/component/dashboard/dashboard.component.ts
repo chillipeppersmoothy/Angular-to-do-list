9d14410acda4042875b300140bdd7fdf4a874bec
Shenoy Kasargod, Aditya <a.shenoy.kasargod@accenture.com>
2023-08-26 22:31:08 +0530

    complete

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../service/crud.service';
+import { Task } from 'src/app/model/task';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  taskObj: Task = new Task();
+  taskArray: Task[] = [];
+  addTaskValue: string = '';
+  changeName: string = 'Add';
+  isEditClicked: boolean = false;
+  updateID:number = 0;
 
+  constructor(private service: CrudService) {}
+
+  ngOnInit(): void {
+    this.taskObj = new Task();
+    this.taskArray = [];
+    this.getAllTasks();
+    this.isEditClicked?this.changeName="Update":this.changeName="Add";
+  }
+
+  onClick(){
+    this.isEditClicked? this.updateTask(): this.addTask() ;
+   }
+
+  getAllTasks(): void {
+    this.service.getAllTasks()
+    .subscribe({
+      next: (data) => this.taskArray = data,
+      error: (err) => alert(err)
+    });
+  }
+
+  addTask(): void {
+    this.taskObj.task_name = this.addTaskValue;
+
+    if(this.addTaskValue === null || this.addTaskValue === '') {
+      return alert('Please enter the task');
+    }
+    
+    this.service.addTask(this.taskObj)
+    .subscribe({
+      next: (data) => this.taskObj = data,
+      error: (err) => alert(err),
+      complete: () => this.ngOnInit()
+    });
+    this.addTaskValue = '';
+  }
+
+  updateTask(): void {
+    this.taskObj = {id:this.updateID,task_name:this.addTaskValue};
+    this.service.editTask(this.taskObj)
+    .subscribe({
+      next: (data) => this.taskObj = data,
+      error: (err) => alert(err),
+      complete: () => this.ngOnInit()
+    });
+    this.updateID = 0;
+    this.addTaskValue = '';
+  }
+
+  editTask(task: Task): void {
+    this.addTaskValue = task.task_name;
+    this.updateID = task.id;
+    this.isEditClicked = true;
+    this.ngOnInit();
+  }
+
+  deleteTask(task: Task): void {
+    this.service.deleteTask(task)
+    .subscribe({
+      next: (data) => this.taskArray.splice(data.id, 1),
+      error: (err) => alert(err),
+      complete: () => this.ngOnInit()
+    });
+  }
+
+  
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Task } from '../model/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  baseUrl: string;
+  constructor(private http:HttpClient) { 
+    this.baseUrl = 'http://localhost:3000/tasks';
+  }
+
+  getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.baseUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.baseUrl, task);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const serviceUrl = `${this.baseUrl}/${task.id}`;
+    return this.http.delete<Task>(serviceUrl);
+  }
+
+  editTask(task: Task): Observable<Task> {
+    const serviceUrl = `${this.baseUrl}/${task.id}`;
+    return this.http.patch<Task>(serviceUrl, task);
+  }
 }
